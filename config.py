2dcc5fa62d2c5ea048b4dfe40ad28f36717dee7f
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-09-02 01:17:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "5340652544"
+SUPPORT_GRP = "unreal_x_support"
+UPDATE_CHNL = "unreal_x_bot"
+OWNER_USERNAME = "SHUBHAM_X_OFFICIAL"
 
